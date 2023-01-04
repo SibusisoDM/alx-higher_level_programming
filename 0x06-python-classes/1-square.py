3244c22f808f69fd6f6667c05142f78922383e8b
SibusisoDM <sdmangwane@gmail.com>
2023-01-03 23:51:55 -0800

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' Class Square '''
+
+
+class Square:
+    '''Represent Square'''
+
+    def __init__(self, size):
+        '''Initializes and reserve data
+        Arg:
+        size (int): The size of the new square
+        '''
+        self.__size = size
