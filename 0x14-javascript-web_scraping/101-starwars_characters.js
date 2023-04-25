831bde4bd69953dde7f46fdd7d6f00b349657081
SibusisoDM <sdmangwane@gmail.com>
2023-04-25 10:23:06 -0700

    tasks

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}`;
+
+request(url, function(error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+    characters.forEach(function(characterUrl) {
+      request(characterUrl, function(error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
